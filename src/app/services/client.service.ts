ae165dc8b142882f5f842a028698734c6698e766
unknown <ra@gmail.com>
2017-11-08 16:20:30 -0800

    AngularFireList

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
+//Rather than .list() returning a FirebaseListObservable, it now returns an AngularFireList<T>.
+//FirebaseObjectObservable to AngularFireObject.
 @Injectable()
 export class ClientService {
 
