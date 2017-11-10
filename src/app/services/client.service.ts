f81d251a4289190dc669e4b0f03065cc0d5eccbb
Raineer <delaritaraineer81@gmail.com>
2017-11-10 14:07:19 -0800

    clientService

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 //Rather than .list() returning a FirebaseListObservable, it now returns an AngularFireList<T>.
 //FirebaseObjectObservable to AngularFireObject.
+import { Observable } from 'rxjs';
 import { Client } from '../models/Client';
 @Injectable()
 export class ClientService {
@@ -13,6 +14,11 @@ export class ClientService {
     public af:AngularFireDatabase
     ) {
     this.clients = this.af.list('/clients') as AngularFireList<Client[]>;
+     }
+
+     getClients(){
+       return this.clients;
+
      }
 
 }
